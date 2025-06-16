f60812e711fb31ec338f32d7e98dc0148c4e7a37
Paulo Vítor Reis <129749259+paulovreis@users.noreply.github.com>
2025-06-16 15:02:34 -0300

    fix: update webhook API port from 3000 to 3010 in Dockerfile and server.js

--- api/server.js
+++ api/server.js
@@ -22,7 +22,7 @@ app.use((req, res, next) => {
 
 app.use("/webhook", webhookRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3010;
 app.listen(PORT, () => {
 	console.log(`Servidor webhook rodando na porta ${PORT}`);
 });
