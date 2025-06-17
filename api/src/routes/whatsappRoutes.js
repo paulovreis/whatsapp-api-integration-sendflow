3460b1a894af46fae236884c735cded75caa1e12
Paulo Vítor Reis <129749259+paulovreis@users.noreply.github.com>
2025-06-17 19:46:04 -0300

    fix: update validation middleware for save-message route to use validateWhatsappMessage

--- api/src/routes/whatsappRoutes.js
+++ api/src/routes/whatsappRoutes.js
@@ -2,10 +2,10 @@
 const express = require("express");
 const router = express.Router();
 
-const { validateSaveMessage } = require("../middlewares/validationMiddleware");
+const { validateWhatsappMessage } = require("../middlewares/validationMiddleware");
 
 module.exports = (whatsappController) => {
   router.get("/message", whatsappController.getMessage.bind(whatsappController));
-  router.post("/save-message", validateSaveMessage, whatsappController.saveMessage.bind(whatsappController));
+  router.post("/save-message", validateWhatsappMessage, whatsappController.saveMessage.bind(whatsappController));
   return router;
 };
