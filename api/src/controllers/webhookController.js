14a3552fd8d20068281960199007bc607aea8288
Paulo Vítor Reis <129749259+paulovreis@users.noreply.github.com>
2025-07-01 13:54:37 -0300

    feat: simplify help messages in message.json and update webhookController to use request data for phone number

--- api/src/controllers/webhookController.js
+++ api/src/controllers/webhookController.js
@@ -48,6 +48,8 @@ class WebhookController {
 							(instance?.connectionStatus === "connected" ||
 								instance?.connectionStatus === "open") &&
 							instance?.name &&
+							//isso aqui é provisório, trocar depois
+							// instance?.name && !instance.name.includes("!") -> original
 							(
 								!instance.name.includes("!") ||
 								instance.name === "!busines1"
@@ -105,14 +107,11 @@ class WebhookController {
 			}
 
 			const data = {
-				number: process.env.TEST_PHONE,//req.body.data.number,
+				number: req.body.data.number,
 				text: text,
 				delay: delay, // Tempo de espera aleatório entre 4 e 10 segundos
 				linkPreview: true, // Habilita a visualização de links
 			};
-
-			console.log("Dados a serem enviados:", data);
-
 
 			if (!data.number || !data.text) {
 				console.error("Campos 'number' e 'text' são obrigatórios.");
