0112310a2d373b395f2556b479cfe845b4ff3a94
Paulo Vítor Reis <129749259+paulovreis@users.noreply.github.com>
2025-07-03 10:22:17 -0300

    feat: remove validation middleware from Evolution API webhook route for tests

--- api/src/routes/webhookRoutes.js
+++ api/src/routes/webhookRoutes.js
@@ -6,6 +6,6 @@ const { validateWebhook, validateEvolutionApiWebhook } = require('../middlewares
 module.exports = (webhookController) => {
   router.post('/', validateWebhook, webhookController.handleWebhook.bind(webhookController));
   // Nova rota EvolutionAPI webhook
-  router.post('/evolutionapi/messages-upsert', validateEvolutionApiWebhook, webhookController.handleEvolutionWebhookFirstMessageResponse.bind(webhookController));
+  router.post('/evolutionapi/messages-upsert', webhookController.handleEvolutionWebhookFirstMessageResponse.bind(webhookController));
   return router;
 };
